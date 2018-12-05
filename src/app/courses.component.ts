9ec4dc925b6cc7052225cd77510d54eff6956eb2
ravindragsingh <arien.641@gmail.com>
2018-12-05 15:22:11 -0500

    first component added

---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+@Component({
+  
+  selector: 'courses',
+  template: '<h2>Courses</h2>'
+})
+export class CoursesComponent { }
