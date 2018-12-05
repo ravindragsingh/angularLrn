9e1628c6a4f4c1b1b68875978f190b3aec8ddfb9
ravindragsingh <arien.641@gmail.com>
2018-12-05 17:08:12 -0500

    directive added

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 @Component({
   
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  template: `<h2>{{title}}</h2>
+  <ul>
+    <li *ngFor= "let course of courses"> 
+    {{course}}
+    </li>
+  </ul>
+  
+  `
 })
-export class CoursesComponent { }
+export class CoursesComponent { 
+title = "List of courses";
+courses = ["ngFor", "ngRx", "ngModule"]
+}
